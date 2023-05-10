9b705d6ebac2573508fb9aec6d2f33b1298e2435
eleander <edward.leander@hotmail.com>
2023-05-10 16:01:23 +0200

    E grade finito
    Co-authored-by: ferransll <ferransll@gmail.com>
    Co-authored-by: KevinArmbruster2013 <KevinArmbruster2013@gmail.com>

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import copy
 import datetime
 import random
+from matplotlib import pyplot as plt
 import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
@@ -70,7 +71,7 @@ resnet = torch.hub.load(
 resnet.eval()
 
 
-def replace_last_layers(model, layers):
+def replace_last_layers(model, layers, unfreeze_norm=False):
     # Make a copy of the model
     model = copy.deepcopy(model)
 
@@ -88,6 +89,13 @@ def replace_last_layers(model, layers):
         param.requires_grad = False
     for param in model.fc.parameters():
         param.requires_grad = True
+
+    # Unfreeze batch norm layers
+    if unfreeze_norm:
+        for m in model.modules():
+            if isinstance(m, (torch.nn.BatchNorm1d, torch.nn.BatchNorm2d)):
+                for param in m.parameters():
+                    param.requires_grad = True
 
     return model
 
@@ -144,6 +152,9 @@ def train(
     epochs=20,
     device="cuda:0",
 ):
+    train_acc = []
+    val_acc = []
+
     model.to(device)
     for epoch in range(epochs):
         current_loss = 0.0
@@ -168,7 +179,7 @@ def train(
             correct += (outputs.argmax(1) == targets).sum().item()
 
         train_loss = current_loss / len(training_loader)
-        train_acc = correct / len(training_loader.dataset)
+        train_acc.append(correct / len(training_loader.dataset))
 
         val_loss = 0.0
         correct = 0
@@ -187,11 +198,43 @@ def train(
                 correct += (outputs.argmax(1) == targets).sum().item()
 
         val_loss /= len(validation_loader)
-        val_acc = correct / len(validation_loader.dataset)
+        val_acc.append(correct / len(validation_loader.dataset))
 
         print(
-            f"Epoch: {epoch+1}/{epochs}.. Train loss: {train_loss:.3f}.. Train acc: {train_acc:.3f}.. Val loss: {val_loss:.3f}.. Val acc: {val_acc:.3f}"
+            f"Epoch: {epoch+1}/{epochs}.. Train loss: {train_loss:.3f}.. Train acc: {train_acc[-1]:.3f}.. Val loss: {val_loss:.3f}.. Val acc: {val_acc[-1]:.3f}"
         )
+
+    return train_acc, val_acc
+
+
+def test_model(model, test_loader, device="cuda:0"):
+    model.to(device)
+    model.eval()
+    correct = 0
+    with torch.no_grad():
+        for batch in test_loader:
+            inputs, targets = batch
+
+            inputs = inputs.to(device)
+            targets = targets.to(device)
+
+            outputs = model(inputs)
+            correct += (outputs.argmax(1) == targets).sum().item()
+
+    return correct / len(test_loader.dataset)
+
+
+def plot_accurcies(train_acc, val_acc, filename=None):
+    plt.plot(train_acc, label="Training")
+    plt.plot(val_acc, label="Validation")
+    plt.legend()
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+
+    if filename:
+        plt.savefig(filename)
+    else:
+        plt.show()
 
 
 # FIRST TEST
@@ -205,8 +248,11 @@ def augment_df(df, p=0.25):
     possible_transformations = [
         transforms.RandomHorizontalFlip(p=1),
         transforms.RandomVerticalFlip(p=1),
-        # transforms.RandomRotation(20),
-        # transforms.RandomRotation(-20),
+        transforms.RandomRotation(20),
+        transforms.RandomRotation(-20),
+        transforms.RandomRotation(40),
+        transforms.RandomRotation(-40),
+        # transforms.RandomCrop(224),
     ]
 
     after_transformation = transforms.Compose(
@@ -238,7 +284,7 @@ training_set = AnimalDataset(train_df, "ClassId")
 validation_set = AnimalDataset(val_df, "ClassId")
 train_loader = DataLoader(training_set, batch_size=batch_size, shuffle=True)
 val_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=True)
-model = replace_last_layers(resnet, [37])
+model = replace_last_layers(resnet, [100, 50, 37], unfreeze_norm=True)
 loss = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=0.001)
 
