b8c3d389e535b56355d817eabb2f9d7dab9291d1
eleander <edward.leander@hotmail.com>
2023-05-12 12:37:37 +0200

    run go brrrr
    Co-authored-by: ferransll <ferransll@gmail.com>
    Co-authored-by: KevinArmbruster2013 <KevinArmbruster2013@gmail.com>

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+echo "Starting E part"
 python e.py
+echo "Starting A part"
 python a.py
+echo "Starting test.py with base model resnet"
 python test.py --model resnet --file_extend base
+echo "Starting test.py with base model vit"
 python test.py --model vit --file_extend base
+echo "Starting test.py with base model resnet and 100 layers"
 python test.py --model resnet --file_extend 100_l --layers 100
+echo "Starting test.py with base model vit and 100 layers"
 python test.py --model vit --file_extend 100_l --layers 100
